030148e72b13473c14d7018cf17581939584548d
habakook <habakook@users.noreply.github.com>
2014-06-29 14:46:39 -0700

    test

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
+    url(r'^zohar/', views.index, name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
