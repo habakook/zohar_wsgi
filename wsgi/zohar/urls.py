5c4b6b5bffb37a4b55b06904ebecbb65b1b70525
habakook <habakook@users.noreply.github.com>
2014-06-29 14:32:02 -0700

    test

--- wsgi/zohar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from zohar import views
-
-urlpatterns = patterns('',
-url(r'^$', views.index, name='index'),
-# ex: /zohar/5/
-url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
-)
