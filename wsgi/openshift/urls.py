02efdcefc444d07ca0ec5b14d34f80901cab406a
habakook <habakook@users.noreply.github.com>
2014-07-01 21:36:50 -0700

    import views

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
-
+from zohar import views
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'views.home', name='home'),
-    url(r'^$', 'views.index', name='index'),
+    url(r'^$', views.index, name='index'),
     # ex: /zohar/5/
-    url(r'^(?P<book_number>\d+)/$', 'views.book', name='view_book'),
+    url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
 
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
