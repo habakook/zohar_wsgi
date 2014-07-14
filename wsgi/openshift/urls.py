8658149420b2f562dc68817f70bdbc56afd4e846
habakook <habakook@users.noreply.github.com>
2014-07-13 22:45:44 -0700

    fix import error

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+import views
 
-from zohar import views
+admin.autodiscover()
 
 urlpatterns = patterns('',
-url(r'^$', views.index, name='index'),
-# ex: /zohar/5/
-#url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
+    url(r'^$', views.index, name='index'),
+    # ex: /zohar/5/
+    #url(r'^(?P<book_number>\d+)/$', views.book, name='view_book'),
 )
